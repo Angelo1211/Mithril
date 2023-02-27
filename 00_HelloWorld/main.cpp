a1eb2ecec6f05c1c9a5b24fbf29574deab03e4b9
Angel Ortiz <angelo12@vt.edu>
2023-02-27 22:09:43 +0000

    First commit!
    Not sure if I'm ignoring the right files for an xcode project!

---
+++ 00_HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  00_HelloWorld
+//
+//  Created by Angel Ortiz on 2/27/23.
+//
+
+#include <stdio.h>
+
+int
+main(int arg_count, char *arg_strings[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
